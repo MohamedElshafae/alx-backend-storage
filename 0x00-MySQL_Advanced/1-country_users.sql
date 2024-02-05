89b01a60ee6229045bbc4638e714ff513fdce554
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-02-05 09:58:49 +0200

    1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a table users
+
+CREATE TABLE IF NOT EXISTS users(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name varchar(255),
+  country ENUM('US', 'CA', 'MX') NOT NULL
+);
