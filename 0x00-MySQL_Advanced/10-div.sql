87310ee139b0d56cab71b08cc249e50c05f42bae
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-02-06 18:46:56 +0200

    done

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,16 +1,15 @@
 -- FUNCTION SafeDiv
 
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS FLOAT
 BEGIN
-    DECLARE result INT;
-    
-    -- Check if the divisor is 0
+    DECLARE result FLOAT;
+
     IF b = 0 THEN
         SET result = 0;
     ELSE
         SET result = a / b;
     END IF;
-    
+
     RETURN result;
 END;
--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,8 +1,7 @@
 --script that creates a view need_meeting that lists all students 
--- that have a score under 80 (strict) and no last_meeting or more than 1 month.
+-- that have a score under 80 (strict) and no last_meeting or more than 1 MONTH
 
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
-WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
